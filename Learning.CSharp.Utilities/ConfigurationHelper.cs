526ede3e284919c292ed31866bd9af749c555d63
vishipayyallore <vishipayyallore@yahoo.com>
2015-10-07 19:07:56 +0530

    AAA

---
+++ Learning.CSharp.Utilities/ConfigurationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.CSharp.Utilities
+{
+
+    public class ConfigurationHelper
+    {
+        #region Methdos.
+        public static string GetConfigurationValue(string configurationItemKey)
+        {
+            return ConfigurationManager.AppSettings[configurationItemKey] ?? string.Empty;
+        }
+        #endregion
+    }
+
+}
